85c91186e96fa117bfbc49c1499e81fe18efdf81
Jonathan Chan <jonathanchan@Jonathans-MBP.home>
2016-01-01 18:33:50 -0500

    Initial commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by Jonathan Chan on 1/1/16.
+//  Copyright © 2016 Jonathan Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
